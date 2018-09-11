424e4177d3c84384b96c57b21d9bf40c7210fa23
Morikiyo Inoha <morikiyo.inoha@gmail.com>
2018-09-11 12:09:25 +0900

    programmed eratos.rb and rspec-test.

--- eratos.rb
+++ eratos.rb
@@ -1,11 +1,32 @@
 class Eratos
   # arg: positive integer
   def self.generate(arg)
-    [2,3,5,7,11,13,17,19,23,29]
+    max_number = Integer(arg) rescue nil
+    if max_number.nil? || max_number < 2
+      raise ArgumentError, "specify an integer 2 and more to argument."
+    end
+
+    search_list = Range.new(2, max_number).to_a
+    prime_numbers = []
+    search_end_value = Math.sqrt(max_number)
+    while search_list[0] <= search_end_value
+      prime_numbers << search_list.delete_at(0)
+      search_list.delete_if {|num| num % prime_numbers.last == 0 }
+    end
+    prime_numbers.concat(search_list)
   end
 end
 
 ### Command Line
 if $0 == __FILE__
-  puts Eratos.generate(30).join(", ")
+  if ARGV.length != 1
+    puts "usage: ruby #{File.basename($0)} number"
+    exit
+  end
+
+  begin
+    puts Eratos.generate(ARGV[0]).join(", ")
+  rescue => ex
+    puts ex.message
+  end
 end
--- spec/eratos_spec.rb
+++ spec/eratos_spec.rb
@@ -2,6 +2,68 @@ require 'spec_helper'
 
 describe Eratos do
   describe '#generate' do
-    it { expect(Eratos.generate(30)).to eq [2,3,5,7,11,13,17,19,23,29]}
+    context 'argument validation test' do
+      example 'minimam prime number list' do
+        expect(Eratos.generate(2)).to eq [2]
+        expect(Eratos.generate('2')).to eq [2]
+      end
+      example 'argument error' do
+        errmsg = "specify an integer 2 and more to argument."
+        expect{ Eratos.generate(1) }.to raise_error(ArgumentError, errmsg)
+        expect{ Eratos.generate(-2) }.to raise_error(ArgumentError, errmsg)
+        expect{ Eratos.generate(nil) }.to raise_error(ArgumentError, errmsg)
+        expect{ Eratos.generate("") }.to raise_error(ArgumentError, errmsg)
+        expect{ Eratos.generate("x") }.to raise_error(ArgumentError, errmsg)
+      end
+    end
+
+    context 'loop condition test' do
+      example 'boundaly value of search end value' do
+        expect(Eratos.generate(4)).to eq [2,3]
+      end
+      example 'greater than boundaly value' do
+        expect(Eratos.generate(3)).to eq [2,3]
+      end
+      example 'less than boundaly value' do
+        expect(Eratos.generate(5)).to eq [2,3,5]
+      end
+    end
+
+    context 'normal operation' do
+      example 'prime numbers less than specified number.' do
+        expected = [2,3,5,7,11,13,17,19,23,29]
+        expect(Eratos.generate(30)).to eq expected
+
+        expected = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113]
+        expect(Eratos.generate(120)).to eq expected
+      end
+    end
+  end
+
+  describe "Command Line" do
+    target_file = "eratos.rb"
+    let(:command) { File.join(File.dirname(__FILE__), "..", target_file) }
+
+    context 'invalid count of command-line args' do
+      example 'print usage' do
+        expected = "usage: ruby #{target_file} number\n"
+        expect(`ruby #{command}`).to eq expected
+        expect(`ruby #{command} 2 3`).to eq expected
+      end
+    end
+
+    context 'normal operation' do
+      example 'print to stdout as in the problem example' do
+        expected = "2, 3, 5, 7, 11, 13, 17, 19, 23, 29\n"
+        expect(`ruby #{command} 30`).to eq expected
+      end
+    end
+
+    context 'argument error' do
+      example 'print message as invalid argment' do
+        expected = "specify an integer 2 and more to argument.\n"
+        expect(`ruby #{command} xxx`).to eq expected
+      end
+    end
   end
 end
